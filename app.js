407a6c897919aeeb8ab2c06a9b616e50d6e60493
Mark Hengel <mhengel@locationinsight.com>
2017-05-25 22:45:28 -0600

    Converting a Number to a String

--- app.js
+++ app.js
@@ -1,16 +1,112 @@
-/*
-* i3logix Code Challenge
-* 
-* Please refer to the README.md for challenge questions and complete your challenge below.
-* 
-* Steps:
-*
-* 1. Write your challenge code below.
-* 2. Export a higher order function that will accept arguments for testing
-*/
+'use strict';
+// Converting a Number to a String
 
+// Write code that will accept a number and convert it to the appropriate string representation.
+
+// Basic Requirements:
+
+// Represent numbers to the hundredth position
+// Represent numbers as high as one billion
+// Examples:
+
+// Convert 2523.04 to "Two thousand five hundred twenty-three and 04/100"
+
+
+
+const _ = require('lodash');
 
 // export your function for testing
-module.exports = function higherOrderFunction() {
+module.exports = function higherOrderFunction(args) {
+	if ( (!args && args != 0 )|| typeof args !== 'number') {
+		return 'Please supply a number!';
+	}
+	if (args > 1000000000) {
+		return 'The number is too big!';
+	}
 
+	const origArr = args.toString().split('');
+	let integerArr = origArr.slice();
+	let decimalArr = [];
+	let numStr = '';
+
+	// Storing number text values
+	const zero = 'Zero'
+	const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	const hundred = 'hundred';
+	const hundreds = ['', 'thousand', 'million', 'billion'];
+
+	if( origArr.indexOf('.') != -1 ) { 
+		integerArr = origArr.slice(0 , origArr.indexOf('.')); //Stores all intergers to the left of the decimal
+		decimalArr = origArr.slice(origArr.indexOf('.') + 1); //Stores all numbers to the right of the decimal
+	}
+
+	// "Divide up the arry into groups of three. Reversing the array allows for the leading numbers to be 3 digits or less for calculation the hundreds spot"
+	integerArr = _.chunk(integerArr.reverse(), 3);
+
+	// Loop through each "chunk" in the array starting from the right because the array is reversed
+	_.forEachRight(integerArr, function(value, i) {
+		if (value.length === 3) { // If the chunk has 3 elements then we send it to the hundreds function which will then call the getDoubles function
+			numStr = buildString(numStr, getHundreds(value, i))
+		} else { // If not 3 then we send it to the getDoubles function
+			numStr = buildString(numStr, getDoubles(value, i))
+		}
+	});
+
+	// Creating decimal fraction value if decimal arr has anything in it
+	if (decimalArr.length){
+		let numerator = decimalArr.join('');
+		let denominator = Math.pow(10, decimalArr.length);
+
+		let decimalStr = numStr.length ? 'and ' : '';
+		decimalStr += numerator + '/' + denominator;
+
+		numStr = buildString(numStr, decimalStr);
+	}
+	
+	// Final string return
+	return finalString(numStr);
+
+
+	// Function creation
+	// Returns the final text number. Returning a captialed first letter then adding the remaing characters back in. If string is empty return is 'Zero'
+	function finalString(origStr) {
+		let finalStr = (origStr.charAt(0).toUpperCase() + origStr.slice(1)).trim();
+		return finalStr ? finalStr : zero;
+	}
+
+	// Building the output string. Checking to see if the string has length and if it does adding a space before the next addtion.
+	function buildString(origStr, newStr) {
+		return origStr.trim() + (origStr.length ? ' ' : '' ) + newStr;
+	};
+
+	// Function to return from the ones
+	function getOnes(i){
+		return ones[i];
+	};
+
+	// Everything goes throught the doubles function to determine the nuances between numbers
+	function getDoubles(arr, i){
+		let doubleStr = '';
+		if (arr[1] == 0 || arr.length == 1) { // If array is only one number ( [5] => five )or the second number is a zero ( [3,0,2] => two) get the single number
+			doubleStr = getOnes(arr[0]);
+		} else if (arr[1] == 1) { // Checks to see of the first number is a "1" and if so it returns a "teen" ( [6,7,1] => seventeen )
+			doubleStr = teens[arr[0]];
+		} else { // If not a teen then return the "tens" and get the second number from the ones ( [5,3,2] => Twenty-three ) 
+			doubleStr = tens[arr[1] - 2] + (arr[0] != 0 ? '-' + getOnes(arr[0]) : '');
+		}
+		// Return double string with returend value from getThousands
+		return doubleStr + (doubleStr.length ? ' ' : '' ) + getThousand(arr, i);	
+	};
+
+	// If the chunked array is three digits long we use the 3rd (last) digit since it is reversed. Three digits mean the number will be a "hundred" number but only if there is not a '0' value in that poistion
+	function getHundreds(arr, i){
+		return getOnes(arr[2]) + (arr[2] != 0 ? ' ' + hundred + ' ' : '') + getDoubles(arr, i);
+	};
+
+	// Based on the index of we are looping over in the integerArr we use it to calculate it's value in the hundred array if the array is not == 0
+	function getThousand(arr, j){
+		return arr.join('') == 0 ? '' : hundreds[j];
+	};
 }
--- test/test.js
+++ test/test.js
@@ -4,6 +4,107 @@ const application = require('../app.js');
 describe('Application', () => {
 
   it('returns desired result', () => {
-    assert.deepEqual(application(arguments), 'desired result')
+    assert.deepEqual(application(2523.04), 'Two thousand five hundred twenty-three and 04/100')
+    assert.deepEqual(application(2523), 'Two thousand five hundred twenty-three')
+    assert.deepEqual(application(.04), '04/100')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(2523), 'Two thousand five hundred twenty-three')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(1), 'One')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(7), 'Seven')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(10), 'Ten')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(17.3), 'Seventeen and 3/10')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(43.30), 'Forty-three and 3/10')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(90), 'Ninety')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(10.04), 'Ten and 04/100')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(0), 'Zero')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(123), 'One hundred twenty-three')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(1000), 'One thousand')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(1100), 'One thousand one hundred')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(1234), 'One thousand two hundred thirty-four')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(10000), 'Ten thousand')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(10100), 'Ten thousand one hundred')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(12345), 'Twelve thousand three hundred forty-five')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(100000), 'One hundred thousand')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(100100), 'One hundred thousand one hundred')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(123456), 'One hundred twenty-three thousand four hundred fifty-six')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(1000000), 'One million')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(1000100), 'One million one hundred')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(1100100), 'One million one hundred thousand one hundred')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(1234567), 'One million two hundred thirty-four thousand five hundred sixty-seven')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(10000000), 'Ten million')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(10000100), 'Ten million one hundred')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(12345678), 'Twelve million three hundred forty-five thousand six hundred seventy-eight')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(100000000), 'One hundred million')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(100000001), 'One hundred million one')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(123456789), 'One hundred twenty-three million four hundred fifty-six thousand seven hundred eighty-nine')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(1000000000), 'One billion')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(999999999999.99), 'The number is too big!')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application(9999999999999.99), 'The number is too big!')
+  })
+  it('returns desired result', () => {
+    assert.deepEqual(application('abc'), 'Please supply a number!')
   })
 });
